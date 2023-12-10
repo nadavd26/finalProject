ed39a1ec6a72b41724f076a6cf3d2d67a7e1f29a
nadavd26 <nadav@drors.net>
2023-12-10 11:50:10 +0200

    color update

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,23 @@ import LoginButton from './LoginScreen/components/login';
 import LogoutButton from './components/logout';
 import { useEffect } from 'react';
 import { gapi } from 'gapi-script';
+import TableScreen from './TableScreen/TableScreen';
 
 const clientId = "697357189642-cv95irflcae6i8dm2nidpvokkqtpv62k.apps.googleusercontent.com"
 
 function App() {
-
-  useEffect(() => {
-    function start() {
-      gapi.client.init({
-        clientId: clientId,
-        scope: ""
-      })
-    };
-
-    gapi.load('client:auth2', start);
-  });
-
-  return (
-    <div className="App">
-      <LoginButton />
-      <LogoutButton />
-    </div>
-  );
+  const booleanArray = new Array(48).fill(false);
+  booleanArray[0] = true;
+  booleanArray[5] = true;
+  booleanArray[10] = true;
+  const booleanArray2 = new Array(48).fill(false);
+  booleanArray2[13] = true;
+  booleanArray2[14] = true;
+  booleanArray2[15] = true;
+  booleanArray2[10] = true;
+  booleanArray2[47] = true
+  const workersAndShifts = [{ name: 'Nadav\n214437550', shifts: booleanArray }, { name: 'Nisim\n214437550', shifts: booleanArray2 }]
+  return <TableScreen workersAndShifts={workersAndShifts}/>
 }
 
 export default App;
