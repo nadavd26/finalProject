d012c5b8a91303cb5d73289c5b680aa285a36607
nadavd26 <nadav@drors.net>
2024-04-10 16:52:05 +0300

    d

--- client/src/TableScreen/Edit/components/NonEditableCell.js
+++ client/src/TableScreen/Edit/components/NonEditableCell.js
@@ -4,8 +4,7 @@ export default function NonEditableCell({ value, rowIndex, columnIndex }) {
 
     return (
         <td
-            id={(columnIndex == undefined || columnIndex == 0) ? 'first-column' : 'zzzzzzzzzzzzz'}
-            // id={`cell-${rowIndex}-${columnIndex}`}
+            id={(columnIndex == undefined || columnIndex == 0) ? 'first-column' : ''}
             className={`cell100 ${(columnIndex == 0  || columnIndex == undefined)? 'first-columns' : 'last-columns'}`}
             style={cellStyle} // Apply the style object here
             contentEditable="false"
