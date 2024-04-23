17a4d9bfa17d4c681793bf1da4ec73597a451e91
nadavd26 <nadav@drors.net>
2024-04-23 20:20:21 +0300

    index coulmn

--- client/src/TableScreen/Edit/EditTable2/Table.js
+++ client/src/TableScreen/Edit/EditTable2/Table.js
@@ -19,7 +19,8 @@ const TableRow = ({rowIndex,row,color,generateWorkerList, onCellEdit, getLineInf
     return (
         <>
             <tr className="row100 body last-rows" id="table-row">
-                <NonEditableCell value={row[0]} rowIndex={rowIndex} coloumnIndex={0} color={color} additionalClass={"no-left"} shiftIndex={row[5]}/>
+            <NonEditableCell value={rowIndex} rowIndex={rowIndex} coloumnIndex={0} color={color} shiftIndex={row[5]}/>
+                <NonEditableCell value={row[0]} rowIndex={rowIndex} coloumnIndex={0} color={color} additionalClass={"no-left second"} shiftIndex={row[5]}/>
                 <NonEditableCell value={row[1]} rowIndex={rowIndex} columnIndex={1} color={color} additionalClass={"no-left"} shiftIndex={row[5]}/>
                 <NonEditableCell value={row[2]} rowIndex={rowIndex} columnIndex={2} color={color} additionalClass={"no-left"} shiftIndex={row[5]}/>
                 <NonEditableCell value={row[3]} rowIndex={rowIndex} columnIndex={3} color={color} additionalClass={"no-left"} shiftIndex={row[5]}/>
@@ -49,11 +50,12 @@ function Table({ content, colors, shiftsPerWorker, workerMap, shiftsInfo, onCell
                         <table className="table table-hover table-striped" id="table">
                             <tbody>
                                 <tr className="row100 body first-row">
-                                    <th className="cell100 col-1 first-column blue static-position" id="first-row-first-col"></th>
+                                    <th className="cell100 col-1 first-column blue static-position" id="first-row-first-col">Index</th>
+                                    <th class="cell100  last-columns blue col-1">Day</th>
                                     <th class="cell100  last-columns blue col-2">Skill</th>
                                     <th class="cell100  last-columns blue col-2">From</th>
                                     <th class="cell100  last-columns blue col-2">Until</th>
-                                    <th class="cell100  last-columns blue col-3">Assigned Worker</th>
+                                    <th class="cell100  last-columns blue col-2">Assigned Worker</th>
                                     <th class="cell100  last-columns blue col-2">Shift Number</th>
                                 </tr>
 
