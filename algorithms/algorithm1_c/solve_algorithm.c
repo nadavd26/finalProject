7c0df7468d3dc7561cf1a47e671e42a3320b986b
atki2 <118188417+atki2@users.noreply.github.com>
2024-09-17 23:26:15 +0300

    Delete algorithms/algorithm1_c directory

--- algorithms/algorithm1_c/solve_algorithm.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "array_operations.h"
-#include "solve_algorithm.h"
-
-#define NUMBER_OFֹ_DAYSֹ_INֹ_Aֹ_WEEK 7
-#define TIME_INTERVALS_A_DAY 48
-
--- algorithms/algorithm1_c/solve_algorithm.h
+++
@@ -1,8 +0,0 @@
-#ifndef SOLVEALGORITHM_H
-#define SOLVEALGORITHM_H
-
-#include "shift.h"
-
-void solve_algorithm1(int ***request_array, OptionalShiftsArray optional_shifts_array, int dim1, int dim2);
-
-#endif
